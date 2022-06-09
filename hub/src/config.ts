766e2558f5ae3905b2d95c63e28c543f75a67900
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-06-09 17:22:50 +0100

    Hub Docker environments update

--- hub/src/config.ts
+++ hub/src/config.ts
@@ -4,16 +4,15 @@ import {NamePathMap} from "./core/loaders/dependencyInjector";
 import {SignOptions} from "jsonwebtoken";
 
 const env = process.env.ENV || 'development';
-let envFile: string;
-if (['development', 'testing'].indexOf(env) !== -1) envFile = '.env';
-else if (env === 'production') envFile = 'prod.env';
-else {
+if (['development', 'testing'].indexOf(env) !== -1) {
+  if (!dotenv.config())
+    throw new Error(`⚠️  Couldn't find .env file. It must be present in ${env} ⚠️`);
+} else {
   logger.error('Invalid environment: must be production, testing or development.');
   process.exit(1);
 }
 
-if (!dotenv.config({path: envFile})) throw new Error("⚠️  Couldn't find .env file  ⚠️");
-const dbType = process.env.DB_TYPE;
+const dbType = process.env.HUB_DB_TYPE;
 if (!dbType) throw `DB_TYPE environment variable must be defined`;
 let dbConfig: LdapConfig | MongoConfig;
 switch (dbType) {
@@ -24,7 +23,7 @@ switch (dbType) {
     dbConfig = {
       adminPwd: (process.env.LDAP_ADMIN_PASSWORD as string).trim(),
       orgName: (process.env.LDAP_ORGANISATION as string).trim(),
-      urls: (process.env.LDAP_URLS as string).trim().split(','),
+      urls: (process.env.HUB_LDAP_URLS as string).trim().split(','),
       orgDomain,
       deps: {
         ldap: {
@@ -35,9 +34,9 @@ switch (dbType) {
     } as LdapConfig;
     break;
   case"mongo":
-    if (!process.env.MONGODB_URL) logger.error(`You should define a MONGODB_URL environment variable.`);
+    if (!process.env.HUB_MONGODB_URL) logger.error(`You should define a MONGODB_URL environment variable.`);
     dbConfig = {
-      url: 'mongodb://' + (process.env.MONGODB_URL || 'mongo:27017'),
+      url: 'mongodb://' + (process.env.HUB_MONGODB_URL || 'mongo:27017'),
       connected: false,
       deps: {
         user: {
@@ -55,10 +54,10 @@ export default {
 
   api: {
     prefix: '/api',
-    port: Number(process.env.PORT) || 4000,
+    port: Number(process.env.HUB_PORT) || 4000,
     jwt: {
-      accessSecret: process.env.ACCESS_TOKEN_SECRET as string,
-      refreshSecret: process.env.REFRESH_TOKEN_SECRET as string,
+      accessSecret: process.env.HUB_ACCESS_TOKEN_SECRET as string,
+      refreshSecret: process.env.HUB_REFRESH_TOKEN_SECRET as string,
       signOptions: {noTimestamp: true} as SignOptions
     }
   },
